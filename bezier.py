fb9eb4b88666324f7df28e5821229015a1ddeaa1
BozoDozo <clem.bozo@hotmail.com>
2022-04-04 14:52:10 +0200

    ajout de bezier

--- bezier.py
+++ bezier.py
@@ -43,15 +43,10 @@ def trace_beizier_quatre(x0, y0, x1, y1, x2, y2, x3, y3, it, debut=0):
     """
 
     u = debut
-    #Tracer OpenGL le point d'origine selon la varibale debut:
     while(u < 1):
         xi, yi = bary_bezier(x0,y0,x1,y1,x2,y2,x3,y3,u)
         #Tracer OpenGL le point xi, yi
         u += pas
-
-
-
-
 
 
 
@@ -66,26 +61,30 @@ def trace_beizier(liste_points,it):
     if(n < 4):
         print("Pas assez de points")
         return -1
-    #On cherche à savoir le nombre de points de controle
-    # qui n'ont pas de quadruplet
-    n_hors  = (n - 4)%3 #si n_hors donne le nombre de sommets qui ne sont
-                    # pas dans un quadruplet
-    n_dans = n-n_hors
+
+    n_hors  = (n - 4)%3 #n_hors donne le nombre de points qui ne sont
+                        # pas dans un quadruplet
+
+    n_int = n-n_hors
 
     #On trace la courbe pour le cas général
-    for(i in range(n_dans)):
+    for i in range(0, n_int, 3):
         trace_beizier_quatre(*liste_points[i],*liste_points[i+1],
                                     *liste_points[i+2], *liste_points[i+3],it)
 
-    #Il y a 2 points hors du quadruplet
-    if n_hors == 2:
-        trace_beizier_quatre(*liste_points[-4],*liste_points[-3],
-                                    *liste_points[-2], *liste_points[-1],
-                                                                    it, 0.25)
+    #On vérifie que l'on ne se trouve pas dans le cas particulier où
+    # il n'y que 4 points dans la liste
+    if(n != 4):
 
-    #Il y a 1 points hors du quadruplet
-    else if n_hors == 1:
-        trace_beizier_quatre(*liste_points[-4],*liste_points[-3],
-                                    *liste_points[-2], *liste_points[-1],
-                                                                    it, 0.75)
-    #Tracer OpenGL dernier point de la courbe
+        #Il y a 2 points hors des quadruplets
+        if n_hors == 2:
+            trace_beizier_quatre(*liste_points[-4],*liste_points[-3],
+                                        *liste_points[-2], *liste_points[-1],
+                                                                      it, 0.25)
+
+        #Il y a 1 point hors des quadruplets
+        elif n_hors == 1:
+            trace_beizier_quatre(*liste_points[-4],*liste_points[-3],
+                                        *liste_points[-2], *liste_points[-1],
+                                                                       it, 0.75)
+        #Trace sur OpenGL le dernier point de la courbe
