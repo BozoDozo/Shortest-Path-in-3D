6e42e1c05325c213d3011d4b238be99b27e323e0
BozoDozo <clem.boz@hotmail.com>
2022-04-20 11:45:10 +0200

    chargement et sauvegarde d'une matrice de valeurs entières et de  valeurs infini

---
+++ save.py
@@ -0,0 +1,57 @@
+
+
+
+import readline
+from typing import List
+
+
+def ecrire_matrice(mat: list[list[int]], path: str = "output.txt") :
+    """
+    Ecrit une matrice dans un fichier texte avec le formatage 
+    suivant:
+    chaque ligne de la matrice est représenté par une ligne
+    dans le texte avec chaque élément séparé d'un espace
+    """
+    f = open(path, 'w')
+    #On parcourt la matrice ligne par ligne et colone par colone
+    for l in mat:
+        for nb in l:
+            f.write(f"{nb} ")
+        #On saute la ligne pour passer a la nouvelle
+        f.write('\n')
+    
+    return 1
+
+def lire_matrice(path: str = "output.txt"):
+    """
+    Lit un fichier écrit par la fonction ecrire_matrice et renvoie
+    la matrice avec les valeurs associés
+    """
+    f = open(path, 'r')
+    #On parcours toute les lignes du fichiers textes on enlève de la
+    #la ligne le dernier élément qui est \n, on transforme la ligne
+    #en une liste de chaîne de caractères qu'on map dont on map chaque
+    #élément au type entier et on transforme l'objet map en liste
+    #En faisant celà pour chaque ligne on obtient une matrice
+    #return [list(map(int,n[:-1].split())) for n in f.readlines()]
+    mat =[n[:-1].split() for n in f.readlines()]
+    l = len(mat)
+    c = len(mat[0])
+    for i in range(l):
+        for j in range(c):
+            if(mat[i][j] == "inf"):
+                mat[i][j] = float("inf")
+            else:
+                mat[i][j] = int(mat[i][j])
+    return mat
+
+
+if __name__ == "__main__":
+
+    mat = [[1,2,33],[4,58,float('inf')],[7,8,9]]
+    ecrire_matrice(mat,"test.txt")
+    print(mat == lire_matrice("test.txt"))
+    
+
+
+    
