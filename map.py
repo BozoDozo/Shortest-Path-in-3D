be5f90bcb4a36af0377e3e2b0b154e07e6fe3b07
BozoDozo <clem.boz@hotmail.com>
2022-05-05 00:42:30 +0200

    interface tkinter quasiment fini

--- map.py
+++ map.py
@@ -163,168 +163,168 @@ def generation_matrice_terrain(n: int, borne_inf: int = 1, borne_sup: int = 10,
 
     return matrice
 
-# def gestion_poly(matrice):
-#     #faire les poly separer
-#     #recuperation de la longeur
-#     n=len(matrice)
+def gestion_poly(matrice):
+    #faire les poly separer
+    #recuperation de la longeur
+    n=len(matrice)
 
-#     #pour avoir ecart en x y
+    #pour avoir ecart en x y
 
-#     ecarty=0
+    ecarty=0
 
-#     for i in range(n):
-#         ecartx=0
-#         if i>=1:
-#             ecarty=2
-#         for j in range(n):
-#             if j>=1:
-#                 ecartx=2
-#             col=matrice[j][i]
-#             glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [col*0.1, col*0.1, col*0.1, 1.0])
-#             glBegin(GL_POLYGON);
+    for i in range(n):
+        ecartx=0
+        if i>=1:
+            ecarty=2
+        for j in range(n):
+            if j>=1:
+                ecartx=2
+            col=matrice[j][i]
+            glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [col*0.1, col*0.1, col*0.1, 1.0])
+            glBegin(GL_POLYGON);
 
-#             glVertex3f(j*ecartx, i*ecarty, matrice[i][j]);
-#             glVertex3f(j*ecartx, i*ecarty+1, matrice[i][j]);
+            glVertex3f(j*ecartx, i*ecarty, matrice[i][j]);
+            glVertex3f(j*ecartx, i*ecarty+1, matrice[i][j]);
 
-#             glVertex3f(j*ecartx, i*ecarty+1, matrice[i][j]);
-#             glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
+            glVertex3f(j*ecartx, i*ecarty+1, matrice[i][j]);
+            glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
 
-#             glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
-#             glVertex3f(j*ecartx+1, i*ecarty, matrice[i][j]);
+            glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
+            glVertex3f(j*ecartx+1, i*ecarty, matrice[i][j]);
 
-#             glVertex3f(j*ecartx+1, i*ecarty, matrice[i][j]);
-#             glVertex3f(j*ecartx, i*ecarty, matrice[i][j]);
-#             glEnd()
+            glVertex3f(j*ecartx+1, i*ecarty, matrice[i][j]);
+            glVertex3f(j*ecartx, i*ecarty, matrice[i][j]);
+            glEnd()
 
-# def gestion_poly_trx(matrice):
-#         #faire les poly separer
-#         #recuperation de la longeur
-#         n=len(matrice)
+def gestion_poly_trx(matrice):
+        #faire les poly separer
+        #recuperation de la longeur
+        n=len(matrice)
 
-#         #pour avoir ecart en x y
+        #pour avoir ecart en x y
 
-#         ecarty=0
+        ecarty=0
 
-#         for i in range(n):
-#             ecartx=0
-#             if i>=1:
-#                 ecarty=2
-#                 if i==n:
-#                     break
-#             for j in range(n):
+        for i in range(n):
+            ecartx=0
+            if i>=1:
+                ecarty=2
+                if i==n:
+                    break
+            for j in range(n):
 
-#                 if j>=1:
-#                     ecartx=2
-#                 if j==n-1:
-#                     break
+                if j>=1:
+                    ecartx=2
+                if j==n-1:
+                    break
 
-#                 col=matrice[j][i]
-#                 glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [0, 0, 1, 1.0])
-#                 glBegin(GL_POLYGON);
-#                 #00 01
-#                 glVertex3f(j*ecartx+1, i*ecarty, matrice[i][j]);
-#                 glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
+                col=matrice[j][i]
+                glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [0, 0, 1, 1.0])
+                glBegin(GL_POLYGON);
+                #00 01
+                glVertex3f(j*ecartx+1, i*ecarty, matrice[i][j]);
+                glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
 
-#                 glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
-#                 glVertex3f(j*ecartx+2, i*ecarty+1, matrice[i][j+1]);
+                glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
+                glVertex3f(j*ecartx+2, i*ecarty+1, matrice[i][j+1]);
 
-#                 glVertex3f(j*ecartx+2, i*ecarty+1, matrice[i][j+1]);
-#                 glVertex3f(j*ecartx+2, i*ecarty, matrice[i][j+1]);
+                glVertex3f(j*ecartx+2, i*ecarty+1, matrice[i][j+1]);
+                glVertex3f(j*ecartx+2, i*ecarty, matrice[i][j+1]);
 
-#                 glVertex3f(j*ecartx+2, i*ecarty, matrice[i][j+1]);
-#                 glVertex3f(j*ecartx+1, i*ecarty, matrice[i][j]);
-#                 glEnd()
+                glVertex3f(j*ecartx+2, i*ecarty, matrice[i][j+1]);
+                glVertex3f(j*ecartx+1, i*ecarty, matrice[i][j]);
+                glEnd()
 
-# def gestion_poly_try(matrice):
-#         #faire les poly separer
-#         #recuperation de la longeur
-#         n=len(matrice)
-#         #pour avoir ecart en x y
-#         ecarty=0
+def gestion_poly_try(matrice):
+        #faire les poly separer
+        #recuperation de la longeur
+        n=len(matrice)
+        #pour avoir ecart en x y
+        ecarty=0
 
-#         for i in range(n):
-#             ecartx=0
-#             if i>=1:
-#                 ecarty=2
-#                 if i==n-1:
-#                     break
-#             for j in range(n):
-#                 if j>=1:
-#                     ecartx=2
-#                 if j==n:
-#                     break
+        for i in range(n):
+            ecartx=0
+            if i>=1:
+                ecarty=2
+                if i==n-1:
+                    break
+            for j in range(n):
+                if j>=1:
+                    ecartx=2
+                if j==n:
+                    break
 
-#                 col=matrice[j][i]
-#                 glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [1, 0, 0, 1.0])
-#                 glBegin(GL_POLYGON);
-#                 #00 01
-#                 glVertex3f(j*ecartx, i*ecarty+1, matrice[i][j]);
-#                 glVertex3f(j*ecartx, i*ecarty+2, matrice[i+1][j]);
+                col=matrice[j][i]
+                glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [1, 0, 0, 1.0])
+                glBegin(GL_POLYGON);
+                #00 01
+                glVertex3f(j*ecartx, i*ecarty+1, matrice[i][j]);
+                glVertex3f(j*ecartx, i*ecarty+2, matrice[i+1][j]);
 
-#                 glVertex3f(j*ecartx, i*ecarty+2, matrice[i+1][j]);
-#                 glVertex3f(j*ecartx+1, i*ecarty+2, matrice[i+1][j]);
+                glVertex3f(j*ecartx, i*ecarty+2, matrice[i+1][j]);
+                glVertex3f(j*ecartx+1, i*ecarty+2, matrice[i+1][j]);
 
-#                 glVertex3f(j*ecartx+1, i*ecarty+2, matrice[i+1][j]);
-#                 glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
+                glVertex3f(j*ecartx+1, i*ecarty+2, matrice[i+1][j]);
+                glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
 
-#                 glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
-#                 glVertex3f(j*ecartx, i*ecarty+1, matrice[i][j]);
-#                 glEnd()
+                glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
+                glVertex3f(j*ecartx, i*ecarty+1, matrice[i][j]);
+                glEnd()
 
-# def gestion_poly_tr_trigd(matrice):
-#         #faire les poly separer
-#         #recuperation de la longeur
-#         n=len(matrice)
-#         #pour avoir ecart en x y
-#         ecarty=0
+def gestion_poly_tr_trigd(matrice):
+        #faire les poly separer
+        #recuperation de la longeur
+        n=len(matrice)
+        #pour avoir ecart en x y
+        ecarty=0
 
-#         for i in range(n):
-#             ecartx=0
-#             if i>=1:
-#                 ecarty=2
-#                 if i==n-1:
-#                     break
-#             for j in range(n):
-#                 if j>=1:
-#                     ecartx=2
-#                 if j==n-1:
-#                     break
+        for i in range(n):
+            ecartx=0
+            if i>=1:
+                ecarty=2
+                if i==n-1:
+                    break
+            for j in range(n):
+                if j>=1:
+                    ecartx=2
+                if j==n-1:
+                    break
 
-#                 col=matrice[j][i]
-#                 glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [1, 1, 0, 1.0])
-#                 glBegin(GL_POLYGON);
-#                 #00 01
-#                 glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
-#                 glVertex3f(j*ecartx+1, i*ecarty+2, matrice[i+1][j]);
+                col=matrice[j][i]
+                glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [1, 1, 0, 1.0])
+                glBegin(GL_POLYGON);
+                #00 01
+                glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
+                glVertex3f(j*ecartx+1, i*ecarty+2, matrice[i+1][j]);
 
-#                 glVertex3f(j*ecartx+1, i*ecarty+2, matrice[i+1][j]);
-#                 glVertex3f(j*ecartx+2, i*ecarty+1, matrice[i][j+1]);
+                glVertex3f(j*ecartx+1, i*ecarty+2, matrice[i+1][j]);
+                glVertex3f(j*ecartx+2, i*ecarty+1, matrice[i][j+1]);
 
-#                 glVertex3f(j*ecartx+2, i*ecarty+1, matrice[i][j+1]);
-#                 glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
+                glVertex3f(j*ecartx+2, i*ecarty+1, matrice[i][j+1]);
+                glVertex3f(j*ecartx+1, i*ecarty+1, matrice[i][j]);
 
-#                 glEnd()
-#                 glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [0, 1, 1, 1.0])
-#                 glBegin(GL_POLYGON);
-#                 #00 01
-#                 glVertex3f(j*ecartx+2, i*ecarty+2, matrice[i+1][j+1]);
-#                 glVertex3f(j*ecartx+1, i*ecarty+2, matrice[i+1][j]);
+                glEnd()
+                glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [0, 1, 1, 1.0])
+                glBegin(GL_POLYGON);
+                #00 01
+                glVertex3f(j*ecartx+2, i*ecarty+2, matrice[i+1][j+1]);
+                glVertex3f(j*ecartx+1, i*ecarty+2, matrice[i+1][j]);
 
-#                 glVertex3f(j*ecartx+1, i*ecarty+2, matrice[i+1][j]);
-#                 glVertex3f(j*ecartx+2, i*ecarty+1, matrice[i][j+1]);
+                glVertex3f(j*ecartx+1, i*ecarty+2, matrice[i+1][j]);
+                glVertex3f(j*ecartx+2, i*ecarty+1, matrice[i][j+1]);
 
-#                 glVertex3f(j*ecartx+2, i*ecarty+1, matrice[i][j+1]);
-#                 glVertex3f(j*ecartx+2, i*ecarty+2, matrice[i+1][j+1]);
+                glVertex3f(j*ecartx+2, i*ecarty+1, matrice[i][j+1]);
+                glVertex3f(j*ecartx+2, i*ecarty+2, matrice[i+1][j+1]);
 
-#                 glEnd()
+                glEnd()
 
-# def grid_map(matrice_map):
-#     #generation poly separe
-#     gestion_poly(matrice_map)
-#     #generation poly transi
-#     gestion_poly_trx(matrice_map)
-#     gestion_poly_try(matrice_map)
-#     gestion_poly_tr_trigd(matrice_map)
+def grid_map(matrice_map):
+    #generation poly separe
+    gestion_poly(matrice_map)
+    #generation poly transi
+    gestion_poly_trx(matrice_map)
+    gestion_poly_try(matrice_map)
+    gestion_poly_tr_trigd(matrice_map)
 
 if __name__ == "__main__":
 
--- menu.py
+++ menu.py
@@ -1,6 +1,8 @@
+from ast import Global
 from select import select
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import  filedialog
+from turtle import left
 from save import lire_matrice
 from save import ecrire_matrice
 from map import generation_matrice_terrain
@@ -24,7 +26,9 @@ taille_matrice = 10
 borne = 10
 flat_niv = 1
 obstacle_bool = False
-taux_obstacle = 0
+seuil_obstacle = 0
+# Si 0 Dijsktra si 1 A*
+algo_chemin = 0
 #####################
 min_value = 0
 max_value = 10
@@ -36,9 +40,27 @@ depart = None
 arrivee = None
 depart_id = None
 arrivee_id = None
-depart_bool = False
-arrivee_bool = True
 
+texte_aide = """Barre d'outils:
+    -Ouvir -> Pour selectionner et charger une matrice
+    -Sauver -> Sauvegarder la matrice affichée à l'écran
+    -Paramètres -> Permet de modifier les paramètres de génération de matrice et de choisir l'algorithme de recherche de chemin
+    -Générer -> Génère une matrice aléatoire avec les paramètres actuels
+    -Aide -> Affiche l'aide
+
+La Matrice: Selon l'intensité le carré est plus ou moins noir, si valeur infini donc obstacle alors couleur bleue 'eau'
+
+Selection point de départ et d'arrivée: Le clic gauche LMB permet de sélectionner les deux points dans la matrice signalés à l'écran 
+par deux ronds rouge, on ne peut pas sélectionner des valeurs obstacles
+
+Désélection: Le clic droit de la souris permet désélectionner les points de départ et d'arrivée
+
+Ajout d'obstacles: Sans passer par les paramètres on peut rajouter directement des obstacles avec le bouton du milieu de la souris MMB
+    
+Quitter: Quitte le programme
+    
+Chemin: Début de l'algorithme de recherche de chemin, Inactif si les deux points ne sont pas sélectionnés
+    """
 # Fonction Utilitaires
 def maj_min_max():
     """
@@ -87,9 +109,11 @@ def get_color(val: Union[int, float]) -> str:
         color = (50,50,255)
     else:
         norm = normalisation(val)
-        color = shade_color((255, 255, 255), norm)
+        color = shade_color((255, 255, 255), 1-norm)
     return rgb_to_hex(*color)
 
+def circle_to_oval(x: int, y: int, r: int):
+    return (x-r, y-r, x+r, y+r)
 # Fonction pour les boutons
 def ouvrir_fichier():
     """
@@ -112,51 +136,76 @@ def ouvrir_matrice():
         Matrice = lire_matrice(path)
         actu_matrice()
 
-def modif_matrice():
+def modif_parametre():
     """
     Modifie les paramètres de generation de matrice
     et en génère une nouvelle
     """
-    global Matrice
-    # Initialisation des valeurs des tirettes aux paramètres globaux
+    # Initialisation des valeurs des tirettes aux paramètres globaux et du choix de l'algo
+    # de recherche du plus court chemin
     win_2 = tk.Toplevel(win)
     n =  tk.IntVar(win_2, taille_matrice)
     born = tk.IntVar(win_2, borne)
     flat = tk.IntVar(win_2, flat_niv)
     obs = tk.IntVar(win_2, obstacle_bool)
-    cran = tk.IntVar(win_2, taux_obstacle)
+    cran = tk.IntVar(win_2, seuil_obstacle)
+    algo = tk.IntVar(win_2, algo_chemin)
     win_2.title("Generation de Matrice")
+    #Parametre pour la generation de la matrice
     tk.Scale(win_2, label="Taille de la matrice", from_=5, to_=50, variable=n , length="10c", orient="horizontal").pack(side="top")
     tk.Scale(win_2, label="Borne supérieur des valeurs", from_=2, to_=100, variable=born,length="10c", orient="horizontal").pack(side="top")
     tk.Scale(win_2, label="Taux d’aplatissement des valeurs", from_=0, to_=10, variable=flat, length="10c", orient="horizontal").pack(side="top")
     tk.Checkbutton(win_2, text= "Présence d'obstacles", variable=obs).pack(side="top")
     tk.Scale(win_2, label="Seuil des d’obstacles", from_=0, to_=5, variable=cran, length="10c", orient="horizontal").pack(side="top")
+    # Choix Algo chemin
+    radio_frame = tk.Frame(win_2)
+    radio_frame.pack(side="top")
+    tk.Radiobutton(radio_frame, text="Dijkstra", indicatoron=0, variable=algo, value= 0).pack(side="left")
+    tk.Radiobutton(radio_frame, text="A*", indicatoron=0, variable=algo, value= 1).pack(side="left")
+    
+    
     # Génération de la matrice avec les nouveaux paramètres entrés
-    tk.Button(win_2,text="Valider",command=lambda:nouvelle_matrice(n.get(), born.get(), flat.get(), bool(obs.get()), cran.get(), win_2)).pack(side="bottom")
+    tk.Button(win_2,text="Valider",command=lambda:nouvelle_matrice_modifie(n.get(), born.get(), flat.get(), bool(obs.get()), cran.get(), algo.get(), win_2)).pack(side="bottom")
 
-def nouvelle_matrice( taille : int, borne_sup: int, flatness: int, a_obstacle: bool, cran_obstacle: int, win: tk.Toplevel = None,):
+def nouvelle_matrice_modifie( taille : int, borne_sup: int, flatness: int, a_obstacle: bool, cran_obstacle: int, alg: int, win: tk.Toplevel = None,):
     """
     Mise à jour des paramètres globaux et génération d'une nouvelle matrice
     """
-    global Matrice, taille_matrice, borne, flat_niv, obstacle_bool, taux_obstacle
+    global Matrice, taille_matrice, borne, flat_niv, obstacle_bool, seuil_obstacle, algo_chemin
     # Generation de la nouvelle matrice
-    Matrice = generation_matrice_terrain(taille, 1, borne_sup, flatness, a_obstacle, cran_obstacle )
-    print(Matrice)
-    # Mise à jour des paramètres globaux
-    taille_matrice, borne, flat_niv, obstacle_bool, taux_obstacle = taille, borne_sup, flatness, a_obstacle, cran_obstacle
-    print(taille,  borne_sup, flatness,a_obstacle, cran_obstacle)
-    # Destruction de la boîte de modification de matrice
-    actu_matrice()
+    algo_chemin = alg
+    #Si aucun paramètre de creation de matrice n'a été changé alors on ne génère pas de nouvelle matrice
+    if(taille_matrice, borne, flat_niv, obstacle_bool, seuil_obstacle) != (taille, borne_sup, flatness, a_obstacle, cran_obstacle):
+        Matrice = generation_matrice_terrain(taille, 1, borne_sup, flatness, a_obstacle, cran_obstacle )
+        #print(Matrice)
+        # Mise à jour des paramètres globaux
+        taille_matrice, borne, flat_niv, obstacle_bool, seuil_obstacle = taille, borne_sup, flatness, a_obstacle, cran_obstacle
+        #print(taille,  borne_sup, flatness,a_obstacle, cran_obstacle)
+        # Destruction de la boîte de modification de matrice
+        actu_matrice()
     if(win):
         win.destroy()
+
+def nouvelle_matrice():
+    """
+    Génération d'une nouvelle matrice
+    """
+    global Matrice
+    Matrice = generation_matrice_terrain(taille_matrice, 1, borne, flat_niv, obstacle_bool, seuil_obstacle )
+    actu_matrice()
+
 
 def actu_matrice():
     """
     Actualise la matrice afficher à l'écran
     """
-    global depart, arrivee, depart_bool, arrivee_bool, depart_id, arrivee_id
-    depart, arrivee, depart_bool, arrivee_bool, depart_id, arrivee_id = None, None, False, False, None, None
+    global depart, arrivee, depart_id, arrivee_id, cote
+    depart, arrivee,depart_id, arrivee_id = None, None, None, None
     maj_min_max()
+    # Taille de carré en fonction de la taille de la matrice
+    cote = 500/len(Matrice[0])
+    #print(len(Matrice))
+    #print(cote)
     Canva.delete(tk.ALL)
     ht = cote*len(Matrice[0])
     wt = cote*len(Matrice[0])
@@ -164,7 +213,11 @@ def actu_matrice():
     for i in range(len(Matrice[0])):
         for j in range(len(Matrice)):
             couleur = get_color(Matrice[i][j])
-            Canva.create_rectangle(i*cote, j*cote,i*cote+cote, j*cote+cote, fill= couleur, tags=("point", Matrice[i][j], str((i,j))))
+            # Les obstacles et valeurs finis ne sont pas traités de la même manière
+            if(Matrice[i][j] == float("inf")):
+                Canva.create_rectangle(i*cote, j*cote,i*cote+cote, j*cote+cote, fill= couleur, tags=("inf", str((i,j))))
+            else:
+                Canva.create_rectangle(i*cote, j*cote,i*cote+cote, j*cote+cote, fill= couleur, tags=("point", str((i,j))))
 
 # Fonction Selection de points
 
@@ -178,36 +231,45 @@ def print_tags(event = None):
 
 def selec_depart(event = None):
     """
-    Change la couleur du point de départ
+    Sélectionne le point de départ
     """
     global depart_id, depart
     id = Canva.find_withtag("current")
     tags = Canva.gettags(id)
-
-    coords = tags[2].split(',')
-    # Car coords ressemble à "(x, y)""
+    coords = tags[1].split(',')
+    # Car coords ressemble à "(x, y)"
     i = int(coords[0][1:])
     j = int(coords[1][1:-1])
     depart = (i,j)
-    #couleur = get_color(float(tags[1]))
-    depart_id = Canva.create_rectangle(i*cote, j*cote,i*cote+cote, j*cote+cote, fill= "yellow", tags=("depart", Matrice[i][j], str((i,j))))
+    # Calcul de points pour créer un cercle au centre du carré
+    xy_xy = circle_to_oval(i*cote+cote/2, j*cote+cote/2, 0.25*cote)
+    depart_id = Canva.create_oval(*xy_xy, fill="red",tags=("depart", str((i,j))))
     Canva.tag_unbind("point","<1>")
-    Canva.tag_bind("point","<1>",selec_depart)
+    Canva.tag_bind("point","<1>",selec_arrivee)
 
 def selec_arrivee(event = None):
+    """
+    Sélectionne le point d'arrivée
+    """
     global arrivee_id, arrivee
     id = Canva.find_withtag("current")
     tags = Canva.gettags(id)
-    coords = tags[2].split(',')
-    # Car coords ressemble à "(x, y)""
+    coords = tags[1].split(',')
+    # Car coords ressemble à "(x, y)"
     i = int(coords[0][1:])
     j = int(coords[1][1:-1])
     arrivee = (i,j)
-    #couleur = get_color(float(tags[1]))
-    arrivee_id = Canva.create_rectangle(i*cote, j*cote,i*cote+cote, j*cote+cote, fill= "orange", tags=("arrivee", Matrice[i][j], str((i,j))))
-    Canva.tag_unbind("point","<1>")
+    # Calcul de points pour créer un cercle au centre du carré
+    xy_xy = circle_to_oval(i*cote+cote/2, j*cote+cote/2, 0.25*cote)
+    arrivee_id = Canva.create_oval(*xy_xy, fill="red",tags=("arrivee", str((i,j))))
+    Canva.tag_unbind("point", "<1>")
+    Canva.tag_unbind("point", "<2>")
+    bouton_chemin.config(state="normal")
 
 def deselec(event = None):
+    """
+    Désélectionne le point de départ et le point d'arrivée
+    """
     global depart, arrivee, depart_id, arrivee_id
     depart = None
     arrivee = None
@@ -218,12 +280,41 @@ def deselec(event = None):
     depart_id = None
     arrivee_id = None
     Canva.tag_bind("point","<1>", selec_depart)
-# Initialisation des fenêtres
+    Canva.tag_bind("point", "<2>", ajout_obstacle)
+    bouton_chemin.config(state="disabled")
 
+def ajout_obstacle(event = None):
+    """
+    Ajout d'obstacle directement sur le terrain
+    """
+    id = Canva.find_withtag("current")
+    tags = Canva.gettags(id)
+    print(tags)
+    coords = tags[1].split(',')
+    # Car coords ressemble à "(x, y)""
+    i = int(coords[0][1:])
+    j = int(coords[1][1:-1])
+    Canva.delete(id)
+    Canva.create_rectangle(i*cote, j*cote,i*cote+cote, j*cote+cote, fill= rgb_to_hex(*(50,50,255)), tags=("inf", str((i,j))))
+
+def afficher_aide(event = None):
+    """
+    Affiche l'aide
+    """
+    aide_win = tk.Toplevel(win)
+    aide_win.title("Aide")
+    text_frame = tk.Frame(aide_win)
+    frame_button = tk.Frame(aide_win)
+    texte = tk.Text(text_frame)
+    texte.insert(tk.END, texte_aide)
+    texte.pack(side="top", expand=True, fill="both")
+    text_frame.pack(fill="both",expand=True)
+    frame_button.pack(side="bottom",fill =tk.Y)
+    tk.Button(frame_button,text="Ok",command=aide_win.destroy)
+
+# Initialisation des fenêtres
 win = tk.Tk()
 win.title("Plan du terrain")
-
-
 top_frame = tk.Frame(win)
 
 # Chargement d'une matrice à partir d'un fichier texte
@@ -234,15 +325,19 @@ bouton_sauver = tk.Button(
     top_frame, text="Sauver", width=20, command=sauver_matrice)
 # Modification des paramètres de la matrice
 bouton_modif = tk.Button(
-top_frame, text="Modifier", width=20, command=modif_matrice)
+top_frame, text="Paramètres", width=20, command=modif_parametre)
 # Génération d'une nouvelle matrice avec les paramètres actuels
 bouton_gen = tk.Button(
-    top_frame, text="Générer", width=20, command=lambda:nouvelle_matrice(taille_matrice, borne, flat_niv, obstacle_bool, taux_obstacle))
+    top_frame, text="Générer", width=20, command=nouvelle_matrice)
 
 #Aide
-aide = tk.Button(top_frame, text="Aide", width=20)
+aide = tk.Button(top_frame, text="Aide", width=20, command=afficher_aide)
 Canva = tk.Canvas(win)
 
+bottom_frame = tk.Frame(win)
+# Lancement de la recherche de chemin
+bouton_chemin = tk.Button(bottom_frame, text= "Chemin", state="disabled", width=20, height= 5)
+bouton_quitter = tk.Button(bottom_frame, text= "Quitter", width=20, height= 5, command=lambda:exit(0))
 
 # Disposition des Widgets
 bouton_charger.pack(side="left", expand=True)
@@ -253,10 +348,15 @@ aide.pack(side="right",expand=True)
 top_frame.pack(side="top", fill=tk.X)
 Canva.pack(side="top")
 
+bouton_chemin.pack(side="right")
+bouton_quitter.pack(side="left")
+bottom_frame.pack(side="bottom", fill=tk.X)
+
 
 # Bindings
 Canva.tag_bind("point", "<1>", selec_depart)
 Canva.bind_all("<3>", deselec)
+Canva.tag_bind("point","<2>", ajout_obstacle)
 actu_matrice()
 
 tk.mainloop()
